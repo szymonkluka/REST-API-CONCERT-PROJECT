6dcef290511918beaedd29eb14cad0f61a4074ca
Szymon Kluka <szymonkluka@onet.pl>
2023-04-12 12:50:10 +0200

    rest-api

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const seatsRoutes = require('./routes/seats.routes');
 
 app.use(cors());
 app.use(express.urlencoded({ extends: false }));
-app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(express.json());
 
 app.use('/api', testimonialsRoutes);
