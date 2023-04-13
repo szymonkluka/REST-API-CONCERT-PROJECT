495a8db047cf0c710f14d46e080231830f8ad4a8
Szymon Kluka <szymonkluka@onet.pl>
2023-04-13 08:39:37 +0200

    REST_API

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -14,9 +14,8 @@ const rootReducer = combineReducers({
 const store = createStore(
   rootReducer,
   compose(
-		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
+    applyMiddleware(thunk),
+  )
 );
 
 export default store;
